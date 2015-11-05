1a8615c9eff6b18dd4c9568e6914177844fdeea6
msroa <maria.sanchez.roa@gmail.com>
2015-11-05 10:12:24 +0100

    copia a local

--- modulos/Login/controladorLogin.js
+++ modulos/Login/controladorLogin.js
@@ -1,13 +1,25 @@
-app.controller('controladorLogin', function(servicioRest, config, $scope, $http) {
+app.controller('controladorLogin', function(servicioRest, config, $scope, $http, $location) {
     $scope.user={        
         name:'',
         password: ''
-    };   
+    };
+    
 	$scope.login = function () {
 
 		servicioRest.postLogin($scope.user)
 			.then(function(data) {
+            
 				console.log(data);
+            
+            
+            if(data.role=="ADMIN_ROLE"){
+             $location.path('/inicio');
+                /* Cargamos una pantalla de inicio con el menu*/
+                
+            }else{
+  
+            }
+
 			})
 			.catch(function(err) {
 				console.log("Error");
