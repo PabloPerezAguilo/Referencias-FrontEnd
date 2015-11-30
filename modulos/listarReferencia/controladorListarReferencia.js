d9cb29b9d3402993671af257d733b917f5e34ec4
msroa <maria.sanchez.roa@gmail.com>
2015-11-30 12:43:50 +0100

    -Creado: Servicio getRefereciasPendientes y vista/funcionalidad Referencias pendientes. -Estilos body

--- modulos/listarReferencia/controladorListarReferencia.js
+++ modulos/listarReferencia/controladorListarReferencia.js
@@ -1,4 +1,14 @@
-app.controller('controladorListarReferencia', function(servicioRest, config, $scope, $http, $location, $rootScope) {  
+app.controller ('controladorListarReferencia', function (servicioRest, config, $scope, $http, $location, $rootScope) {  
     
-
+    $scope.titulo = 'REFERENCIAS PENDIENTES DE VALIDAR';
+     servicioRest.getReferenciasPendientes().then(
+        function (response) {
+            
+            $scope.referencias = response;
+            /*if($scope.referencias){
+            
+                }*/
+              
+            
+        });
 });
