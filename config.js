9af6ac4d1c09557eb470c62a5473914bdd09b3a0
aGonzalez94 <alejandro.powerfull@gmail.com>
2015-12-22 11:02:55 +0100

    borrar Fecha erronea - en progreso

--- config.js
+++ config.js
@@ -13,12 +13,45 @@ app.constant('config', {
 });
 //Configuracion necesaria para la fecha del DatePicker
 app.config(function($mdDateLocaleProvider) {
-  $mdDateLocaleProvider.formatDate = function(date) {
-    return date ? moment(date).format('D/M/YYYY') : '';
-  };
-  
-  $mdDateLocaleProvider.parseDate = function(dateString) {
-    var m = moment(dateString, 'D/M/YYYY', true);
-    return m.isValid() ? m.toDate() : new Date(NaN);
-  };
-});
+        $mdDateLocaleProvider.firstDayOfWeek = 1;
+    $mdDateLocaleProvider.shortDays = ['D', 'L', 'M', 'X', 'J', 'V', 'S'];
+    $mdDateLocaleProvider.shortMonths = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
+      $mdDateLocaleProvider.formatDate = function(date) {
+          console.log("aaa");
+        return date ? moment(date).format('D/M/YYYY') : '';
+      };
+
+      $mdDateLocaleProvider.parseDate = function(dateString) {
+        var m = moment(dateString, 'D/M/YYYY', true);
+          console.log("bbb");
+        return m.isValid() ? m.toDate() : moment('undefined', 'D/M/YYYY', true).toDate();
+      };
+});/*
+
+ app.factory('focus', function($timeout, $window) {
+    return function(id) {
+      // timeout makes sure that it is invoked after any other event has been triggered.
+      // e.g. click events that need to run before the focus or
+      // inputs elements that are in a disabled state but are enabled when those events
+      // are triggered.
+      $timeout(function() {
+        var element = $window.document.getElementById(id);
+        if(element)
+          element.focus();
+      });
+    };
+  });
+
+app.directive('eventFocus', function(focus) {
+    return function(scope, elem, attr) {
+      elem.on(attr.eventFocus, function() {
+        focus(attr.eventFocusId);
+      });
+
+      // Removes bound events in the element itself
+      // when the scope is destroyed
+      scope.$on('$destroy', function() {
+        elem.off(attr.eventFocus);
+      });
+    };
+  });*/
