e400d817d6c645b2e97fd334547c149d8ff90ece
aGonzalez94 <alejandro.powerfull@gmail.com>
2016-02-24 15:47:40 +0100

    -ahora los elementos del arbol no se pueden arrastrar -busqueda mediante seleccion multiple de elementos de arbol de tecnologias REALIZADO a falta de inicializar los seleccioados si existe alguno

--- modulos/popUp/controladorSeleccionarTecnologias.js
+++ modulos/popUp/controladorSeleccionarTecnologias.js
@@ -3,12 +3,9 @@ app.controller('controladorSeleccionarTecnologias', function ($scope, $mdDialog,
     //Con $document.on() determinamos que, al presionar la tecla backspace (e.which === 8), y si no estamos en un textarea entonces impedimos que la tecla funcione de forma normal, para evitar navegar hacia atras mientras estemos en el popUp
     //Creamos la variable bloqueoActivo para permitir que la tecla backspace vuelva a funcionar de forma normal una vez haya salido del popUp
     var tecSelec = [];
-    if(tecnologiasSelecIniciales != undefined){
-        console.log(tecnologiasSelecIniciales);
-        tecSelec = tecnologiasSelecIniciales;
-    }
     
-    console.log(tecnologiasSelecIniciales);
+    
+    //console.log(tecnologiasSelecIniciales);
     var nodeData;
     var operacion;
     $scope.nodoSeleccionado={};
@@ -23,13 +20,17 @@ app.controller('controladorSeleccionarTecnologias', function ($scope, $mdDialog,
             actualizarArbol(response);
         });
     
-    function actualizarArbol(arbol){                               
+    function actualizarArbol(arbol){ 
+            if(tecnologiasSelecIniciales != undefined){
+                console.log(tecnologiasSelecIniciales);
+                tecSelec = tecnologiasSelecIniciales;
+            }
+        
             recorrerArbol(arbol);                        
             $scope.data = [];                      
-            $scope.data[0] = arbol;                  
+            $scope.data[0] = arbol; 
+        //console.log($scope.data[0]);
     };
-    
-    
     
     function recorrerArbol(response){
         if(response.nodosHijos != null){
@@ -37,9 +38,8 @@ app.controller('controladorSeleccionarTecnologias', function ($scope, $mdDialog,
                 recorrerArbol(response.nodosHijos[i]);
             }
         }
-        else{
-            response.nodosHijos=[];
-        }
+        
+        //console.log($scope.data[0].$element);
     };
     
     $scope.nodoSeleccionado;
@@ -47,69 +47,6 @@ app.controller('controladorSeleccionarTecnologias', function ($scope, $mdDialog,
     // Iniciamos el nodo selleccionado a undefined para indicar que inicialmente no hay ninguno seleccionado
     var elementoSeleccionado=undefined;
     
-    $scope.eventosArbol = {
-        //Cuando salte el evento (la clave), saltará la función de callback que contiene
-        
-        //cuando se intenta arrastrar un nodo a otro sitio. Si se retorna true, se podrá soltar ahí. Si no, no 
-        accept: function(origen, destino, indiceDestino) {
-            
-            // No se podrá arrastrar un nodo raíz ( Si origen.$parentNodeScope != undefined ), ni se podrámover un elemento a nodo raíz ni fuera ( destino.$parent.$modelValue.nombre!=undefined ) ni se podrá mover un nodo hoja u hoja inválida (destino.$nodeScope.$modelValue.clase === 'nodo')
-            
-            // ¡¡¡ IMPORTANTE !!!! se compara con '!=' o '==' en lugar de '!==' y '===' porque a veces es null y otras undefined
-            
-            try{
-               // return true;
-                console.log("DESTINO:");/* Si se trata del elemento que contienen la raíz, será un array. Soi no, no tendrá $modelValue y dará unerror que recogeremos en el catch
-                */
-                return destino.$parent.$modelValue.nombre!=undefined 
-                        && origen.$parentNodeScope != undefined 
-                        && (destino.$nodeScope.$modelValue.clase === 'nodo'
-                        || destino.$nodeScope.$modelValue.clase === 'raiz');
-            }
-            catch(error){
-                //console.error(error);
-                return false;
-            }
-        },
-        
-        //Cuando se ha movido el nodo
-        dropped: function(e) {
-            console.log("dropped");
-            try{
-                // Ponemos el nodo seleccionado a  null para que la introducción de datos desaparezca, ya que se pierde el knodo seleccionado al arrastrar
-                $scope.nodoSeleccionado=null;
-                
-                // Obtenemos el padre del que se ha movido el nodo
-                var padreOrigen = e.source.nodesScope.$parent.$modelValue.nombre;
-                
-                //Obtenemos el padre destino
-                var padreDestino = e.dest.nodesScope.$parent.$modelValue.nombre;
-                
-                //Obtenemos el nodo a mover
-                var nodo= e.source.nodeScope.$modelValue;
-                // Hacer la llamada al back
-                servicioRest.putMoverTecnologia(padreDestino, nodo)
-                .then(function(data) {
-                    //eliminarNodo(scope);
-                    actualizarArbol(data);
-
-                }).catch(function(err) {
-                    utils.popupInfo('',"Error al mover tecnologia.");
-                    console.log("Error al mover tecnologia");
-                    servicioRest.getTecnologias().then(
-                    function (response) {
-                        actualizarArbol(response);
-                    });
-                });   
-            }
-            catch(error){
-                //Se meterá en el catch en caso de que muevas el elemento raíz o que muevas un elemento fuera del raíz.
-                //Como no tiene padre, saltará la excepción de que no existe $modelValue del $parent (NO DEBERÍA PASAR!!!!!)
-                console.error(error);
-            }
-            
-        }
-    };
     
 
     
@@ -120,10 +57,11 @@ app.controller('controladorSeleccionarTecnologias', function ($scope, $mdDialog,
         var tipo;
         nodeData=nodo;
         var elemActual = elem;//   elem.childNodes()[0];
+        //console.log("AQUI",elemActual);
+            //console.log("AQUI",elemActual.$element[0]);
         if(elemActual.$element[0].classList.contains("elementoSeleccionado")){
-            console.log("hola");
             elemActual.$element.removeClass("elementoSeleccionado");
-            console.log("indexPadre",tecSelec.indexOf(elemActual.$modelValue.nombre));
+            //console.log("indexPadre",tecSelec.indexOf(elemActual.$modelValue.nombre));
             tecSelec.splice(tecSelec.indexOf(elemActual.$modelValue.nombre),1);
         }else{
             elemActual.$element.addClass("elementoSeleccionado");
@@ -132,23 +70,25 @@ app.controller('controladorSeleccionarTecnologias', function ($scope, $mdDialog,
         
         while(elemActual.hasChild()){
             elemActual = elemActual.childNodes()[0];
-            console.log("AQUI",elemActual.$element[0].classList);
-            if(elemActual.$element[0].classList.contains("elementoSeleccionado")){
-                console.log("selec");
-                elemActual.$element.removeClass("elementoSeleccionado");
-                console.log("indexHijo",tecSelec.indexOf(elemActual.$modelValue.nombre));
+            //console.log("AQUI",elemActual);
+            //console.log("AQUI",elemActual.$element[0].firstElementChild.classList);
+            //console.log("AQUI",elemActual.$element[0].classList);
+            if(elemActual.$element[0].firstElementChild.classList.contains("elementoSeleccionado")){
+                //console.log("selec");
+                elemActual.$element[0].firstElementChild.classList.remove("elementoSeleccionado");
+                //console.log("indexHijo",tecSelec.indexOf(elemActual.$modelValue.nombre));
                 tecSelec.splice(tecSelec.indexOf(elemActual.$modelValue.nombre),1);
             }else{
-                console.log("no selec");
-                elemActual.$element.addClass("elementoSeleccionado");
+                //console.log("no selec");
+                elemActual.$element[0].firstElementChild.classList.add("elementoSeleccionado");
                 tecSelec.push(elemActual.$modelValue.nombre);
             }
             //tecSelec.push(elemActual.$modelValue.nombre);
             //elemActual.$element.addClass("elementoSeleccionado");
-            console.log(elemActual.$element);
-            console.log(elemActual.$modelValue);
+            //console.log(elemActual.$element);
+            //console.log(elemActual.$modelValue);
         }
-        console.log(tecSelec);
+        //console.log(tecSelec);
     
     };
         
